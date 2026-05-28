b4e6cc4c2f449339fdce15e33484bf8c106ecfc9
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-05-28 13:57:03 +0700

    todo migration, model, controller and api setup for backend

---
+++ back-end/routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// get all todos
+Route::get('/todos', [TodoController::class, 'index']);
+
+// create todo
+Route::post('/todos', [TodoController::class, 'store']);
+
+// update todo
+Route::patch('/todos/${todo}', [TodoController::class, 'update']);
+
+// delete todo
+Route::delete('/todos/${todo}', [TodoController::class, 'destroy']);
+
+// Route::apiResource('todos', TodoController::class);
